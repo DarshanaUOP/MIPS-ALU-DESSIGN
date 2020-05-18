c5d9018a20a44047ab9182f023251f83b9d49a93
DarshanaUOP <46531949+DarshanaUOP@users.noreply.github.com>
2020-05-19 01:05:31 +0530

    TEST ALL OPERATIONS

--- MIPSALU-ALU.v
+++ MIPSALU-ALU.v
@@ -88,14 +88,132 @@ initial begin
 
 /* TEST CASSES FOR OR OPERATION */	
 	#20	ALUCtl	=	4'h1;		// 1 - OR 
+// SET A AND B BOTH ZERO
+	#20	A	=	32'h0;
+	#20	B	=	32'h0;
 
+	// SET A AND B BOTH ONE
+	#20	A	=	32'h1;
+	#20	B	=	32'h1;
+
+	// REMAIN A ONE AND CHANGE B TO TWO
+	#20	A	=	32'h1;
+	#20	B	=	32'h2;
+
+	// CHANGE A INTO 3 AND B INTO 6 
+	#20	A	=	32'h3;
+	#20	B	=	32'h6;
+
+	// CHANGE A INTO 7 AND B INTO 4
+	#20	A	=	32'h7;
+	#20	B	=	32'h4;
+
+	// CHANGE A INTO 4'ha (TEN) AND B INTO 4'hc (12)
+	#20	A	=	32'ha;
+	#20	B	=	32'hc;
+/* TEST CASSES FOR ADD OPERATION */
 	#20	ALUCtl	=	4'b0010;	// 2 - ADD
-	
+	// SET A AND B BOTH ZERO
+	#20	A	=	32'h0;
+	#20	B	=	32'h0;
+
+	// SET A AND B BOTH ONE
+	#20	A	=	32'h1;
+	#20	B	=	32'h1;
+
+	// REMAIN A ONE AND CHANGE B TO TWO
+	#20	A	=	32'h1;
+	#20	B	=	32'h2;
+
+	// CHANGE A INTO 3 AND B INTO 6 
+	#20	A	=	32'h3;
+	#20	B	=	32'h6;
+
+	// CHANGE A INTO 7 AND B INTO 4
+	#20	A	=	32'h7;
+	#20	B	=	32'h4;
+
+	// CHANGE A INTO 4'ha (TEN) AND B INTO 4'hc (12)
+	#20	A	=	32'ha;
+	#20	B	=	32'hc;
+
+/* TEST CASSES FOR SUBSTRACT OPERATION */
 	#20	ALUCtl	=	4'b0110;	// 6 - Substrct
+// SET A AND B BOTH ZERO
+	#20	A	=	32'h0;
+	#20	B	=	32'h0;
 
-	#20	ALUCtl	=	4'b0111;	// 7 - Set on Less Than (SLT)
+	// SET A AND B BOTH ONE
+	#20	A	=	32'h1;
+	#20	B	=	32'h1;
 
-	#20	ALUCtl	=	4'b1100;	// 12 - NOR
+	// REMAIN A ONE AND CHANGE B TO TWO
+	#20	A	=	32'h1;
+	#20	B	=	32'h2;
+
+	// CHANGE A INTO 3 AND B INTO 6 
+	#20	A	=	32'h3;
+	#20	B	=	32'h6;
+
+	// CHANGE A INTO 7 AND B INTO 4
+	#20	A	=	32'h7;
+	#20	B	=	32'h4;
+
+	// CHANGE A INTO 4'ha (TEN) AND B INTO 4'hc (12)
+	#20	A	=	32'ha;
+	#20	B	=	32'hc;
+
+/* TEST CASSES FOR 'SET ON LESS THAN OPERATION' */ 
+	#20	ALUCtl	=	4'h7;	// 7 - Set on Less Than (SLT)
+// SET A AND B BOTH ZERO
+	#20	A	=	32'h0;
+	#20	B	=	32'h0;
+
+	// SET A AND B BOTH ONE
+	#20	A	=	32'h1;
+	#20	B	=	32'h1;
+
+	// REMAIN A ONE AND CHANGE B TO TWO
+	#20	A	=	32'h1;
+	#20	B	=	32'h2;
+
+	// CHANGE A INTO 3 AND B INTO 6 
+	#20	A	=	32'h3;
+	#20	B	=	32'h6;
+
+	// CHANGE A INTO 7 AND B INTO 4
+	#20	A	=	32'h7;
+	#20	B	=	32'h4;
+
+	// CHANGE A INTO 4'ha (TEN) AND B INTO 4'hc (12)
+	#20	A	=	32'ha;
+	#20	B	=	32'hc;
+
+/* TEST CASSES FOR 'NOR' OPERATION */
+	#20	ALUCtl	=	4'hc;	// 12 - NOR
+// SET A AND B BOTH ZERO
+	#20	A	=	32'h0;
+	#20	B	=	32'h0;
+
+	// SET A AND B BOTH ONE
+	#20	A	=	32'h1;
+	#20	B	=	32'h1;
+
+	// REMAIN A ONE AND CHANGE B TO TWO
+	#20	A	=	32'h1;
+	#20	B	=	32'h2;
+
+	// CHANGE A INTO 3 AND B INTO 6 
+	#20	A	=	32'h3;
+	#20	B	=	32'h6;
+
+	// CHANGE A INTO 7 AND B INTO 4
+	#20	A	=	32'h7;
+	#20	B	=	32'h4;
+
+	// CHANGE A INTO 4'ha (TEN) AND B INTO 4'hc (12)
+	#20	A	=	32'ha;
+	#20	B	=	32'hc;
 
 
 end
